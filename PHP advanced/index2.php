e83191c299314675dd36e2faea4a01c6703fd5f6
Raz Elbaz <93310416+RazElbaz@users.noreply.github.com>
2023-10-23 14:17:21 +0300

    Add files via upload

--- PHP advanced/index2.php
+++ PHP advanced/index2.php
@@ -1,17 +1,31 @@
 <?php
-$var=4;
+if(isset($_POST["name"])){
+    $var=$_POST["name"];
+}
+else{
+    $var=array("Tony", "Steve", "Jaff", "Fraklin");
+}
+
 $var3=6;
 $var2="<html>
 <head></head>
 <body><h1>Hello</h1>
-<form action='http://localhost/lessons/index2.php' method='GET'>
+<form action='http://localhost/lessons/index2.php' method='POST'>
 <input type='text' name='name'></input><br  />
 <input type='submit' value='Submit'>
 </form>
 </body>
 </html>";
 
-echo $var;
+array_push($var, "Billy");
+array_pop($var);
+$var[2]="Arnold";
+
+foreach($var as $index){
+    echo $index;
+    echo "<br>";
+}
+print_r($var);
 echo $var2;
 
 
