ea89bad2aa120874d95f82c9e7cce708adfb26d1
Raz Elbaz <93310416+RazElbaz@users.noreply.github.com>
2023-10-23 13:08:52 +0300

    Update index.php

--- PHP advanced/index.php
+++ PHP advanced/index.php
@@ -1,15 +1,15 @@
 <?php
-$var=4;
-$var3=6;
-$var2="<html>
-<head></head>
-<body><h1>Hello</h1></body>
-</html>";
+$newvar="New lesson";
+$check=3;
 
-echo $var+$var3;
-echo $var2;
-
-
-
+if($check==5){
+echo $newvar;
+}
+elseif($check>=4){
+    echo "second result";
+}
+else{
+    echo "third result";
+}
 
 ?>
